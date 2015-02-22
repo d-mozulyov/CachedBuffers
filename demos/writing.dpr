f56d2fd41fb90f7e200ed1d5eaa31a0a08bbba29
d-mozulyov <softforyou@inbox.ru>
2015-02-22 22:28:52 +0300

    Benchmark refactoring

--- demos/writing.dpr
+++ demos/writing.dpr
@@ -1,114 +1,120 @@
 program writing;
 
-
 {$APPTYPE CONSOLE}
+
 // compiler options
-{$V+}{$B-}{$X+}{$T+}{$P+}{$H+}{$J-}{$MINENUMSIZE 1}
+{$U-}{$V+}{$B-}{$X+}{$T+}{$P+}{$H+}{$J-}{$Z1}{$A4}
 {$ifndef VER140}
   {$WARN UNSAFE_CODE OFF}
   {$WARN UNSAFE_TYPE OFF}
   {$WARN UNSAFE_CAST OFF}
 {$endif}
-{$O+}{$R-}{$I-}{$Q-}
+{$O+}{$R-}{$I-}{$Q-}{$W-}
 
 uses
-  Windows, SysUtils, CachedBuffers, Classes;
+  Windows, SysUtils, Classes, CachedBuffers;
 
-type
-  EStopBenchmark = class(Exception);
-{$if CompilerVersion < 19}
-  NativeInt = Integer;
-{$ifend}
 
 const
   CORRECT_FILE_NAME = 'correct_file.txt';
 
+{$if CompilerVersion < 19}
+type
+  NativeInt = Integer;
+  NativeUInt = Cardinal;
+{$ifend}
 
-procedure ConsoleWrite(const StrFmt: AnsiString; const Args: array of const; const CrlfCount: integer=1);
+procedure ConsoleWrite(const StrFmt: string; const Args: array of const;
+  const CRLFCount: Integer = 1); overload;
 var
-  i, Len: integer;
-  Buf: AnsiString;
-  S: PAnsiChar;
+  i, Len: Integer;
+  S: PChar;
+  Buf: string;
 begin
   Buf := Format(StrFmt, Args);
   Len := Length(Buf);
   S := pointer(Buf);
+  {$ifNdef UNICODE}
   Windows.CharToOemBuffA(S, S, Len);
+  {$endif}
 
-  for i := 0 to Len-1 do
+  for i := 0 to Len - 1 do
   if (S[i] = #13) then S[i] := #10;
 
-  if (CrlfCount <= 0) then
+  if (CRLFCount <= 0) then
   begin
     Write(Buf);
   end else
   begin
     Writeln(Buf);
-    for i := 2 to CrlfCount do Writeln;
+    for i := 2 to CRLFCount do Writeln;
   end;
 end;
 
-procedure CompareFiles(const FileName_1, FileName_2: string; const compact_info: boolean=true);
+procedure ConsoleWrite(const Text: string; const CRLFCount: Integer = 1); overload;
+begin
+  ConsoleWrite(Text, [], CRLFCount);
+end;
+
+procedure CompareFiles(const FileName1, FileName2: string;
+  const CompactInfo: Boolean = True);
 const
   BUFFER_SIZE = 256 * 1024;
 var
   Name1, Name2: string;
-  F1, F2: integer;
-  S1, S2: int64;
+  F1, F2: THandle;
+  S1, S2: Int64;
 
-  Reading: integer;
-  SameFiles: boolean;
+  Reading: Integer;
+  SameFiles: Boolean;
 
-  Buffer1: array[0..BUFFER_SIZE-1] of byte;
-  Buffer2: array[0..BUFFER_SIZE-1] of byte;
+  Buffer1: array[0..BUFFER_SIZE-1] of Byte;
+  Buffer2: array[0..BUFFER_SIZE-1] of Byte;
 
-  function OpenReadingFile(const FileName: string; out Size: int64): integer;
+  function OpenReadingFile(const FileName: string; out Size: Int64): THandle;
   begin
     Result := FileOpen(FileName, fmOpenRead or fmShareDenyNone);
-    if (Result < 0) then raise Exception.CreateFmt('Cannot read file:'#13'%s', [FileName]);
+    if (NativeInt(Result) < 0) then
+      raise Exception.CreateFmt('Cannot read file:'#13'%s', [FileName]);
 
-    Size := FileSeek(Result, int64(0), FILE_END);
+    Size := FileSeek(Result, Int64(0), FILE_END);
     FileSeek(Result, 0, FILE_BEGIN);
   end;
 
-  procedure ReadBuffer(const F: integer; var Buffer; const Count: integer);
+  procedure ReadBuffer(const F: THandle; var Buffer; const Count: Integer);
   begin
     if (Count <> FileRead(F, Buffer, Count)) then
-    raise Exception.Create('Stream read error');
+      raise Exception.Create('Stream read error');
   end;
 
-  procedure Error();
-  begin
-    raise EStopBenchmark.Create('');
-  end;
 begin
-  Name1 := ExtractFileName(FileName_1);
-  Name2 := ExtractFileName(FileName_2);
-  if (compact_info) then ConsoleWrite('comparing with correct file... ', [], 0)
+  Name1 := ExtractFileName(FileName1);
+  Name2 := ExtractFileName(FileName2);
+  if (CompactInfo) then ConsoleWrite('comparing with correct file... ', 0)
   else ConsoleWrite('Files comparing "%s" and "%s"... ', [Name1, Name2], 0);
 
-  if (not FileExists(FileName_1)) then
+  if (not FileExists(FileName1)) then
   begin
     ConsoleWrite('"%s" not found', [Name1]);
-    Error();
+    Abort;
   end;
-  if (not FileExists(FileName_2)) then
+  if (not FileExists(FileName2)) then
   begin
     ConsoleWrite('"%s" not found', [Name2]);
-    Error();
+    Abort;
   end;
 
-  F1 := OpenReadingFile(FileName_1, S1);
+  F1 := OpenReadingFile(FileName1, S1);
   try
-    F2 := OpenReadingFile(FileName_2, S2);
+    F2 := OpenReadingFile(FileName2, S2);
     try
       if (S1 <> S2) then
       begin
         ConsoleWrite('different sizes: %d and %d', [S1, S2]);
-        Error();
+        Abort;
       end;
 
-      SameFiles := true;
+      SameFiles := True;
       while (S1 <> 0) do
       begin
         if (S1 < BUFFER_SIZE) then Reading := S1
@@ -118,8 +124,8 @@ begin
         ReadBuffer(F2, Buffer2, Reading);
         if (not CompareMem(@Buffer1, @Buffer2, Reading)) then
         begin
-          SameFiles := false;
-          break;
+          SameFiles := False;
+          Break;
         end;
 
         S1 := S1 - Reading;
@@ -127,10 +133,12 @@ begin
 
       if (not SameFiles) then
       begin
-        ConsoleWrite('NOT EQUAL !!!', []);
-        Error();
-      end
-      else ConsoleWrite('binary equal.', []);
+        ConsoleWrite('NOT EQUAL !!!');
+        Abort;
+      end else
+      begin
+        ConsoleWrite('binary equal.');
+      end;
     finally
       FileClose(F2);
     end;
@@ -142,26 +150,26 @@ end;
 
 const
   CRLF_VALUE = 13 or (10 shl 8);
-  CRLF: word = CRLF_VALUE;
+  CRLF: Word = CRLF_VALUE;
   STRINGS_COUNT = 1000;
   ITERATIONS_COUNT = 22000;// div 100;
 
 var
-  FileId: dword = 0;
-  
+  ProcNumber: Cardinal = 0;
+
   STRINGS: array[1..STRINGS_COUNT] of record
-    Len: integer;
+    Len: Integer;
     S: AnsiString;
   end;
 
   procedure INITIALIZE_STRINGS;
   var
-    i: integer;
+    i: Integer;
   begin
     for i := Low(STRINGS) to High(STRINGS) do
     with STRINGS[i] do
     begin
-      S := IntToStr(i);
+      S := AnsiString(IntToStr(i));
       Len := Length(S);
     end;
   end;
@@ -173,14 +181,14 @@ type
 // standard way to write data to Stream
 procedure StreamWriter(Stream: TStream);
 var
-  iteration, i: integer;
+  iteration, i: Integer;
 begin
   for iteration := 1 to ITERATIONS_COUNT do
   for i := Low(STRINGS) to High(STRINGS) do
   with STRINGS[i] do
   begin
-    Stream.Write(pointer(S)^, Len);
-    Stream.Write(CRLF, sizeof(CRLF));
+    Stream.Write(Pointer(S)^, Len);
+    Stream.Write(CRLF, SizeOf(CRLF));
   end;
 end;
 
@@ -197,7 +205,7 @@ begin
   end;
 end;
 
-// write to TMemoryStream and SaveToFile()
+// write to TMemoryStream and SaveToFile
 procedure MemoryStreamWriter(const FileName: string);
 var
   Stream: TMemoryStream;
@@ -214,9 +222,9 @@ end;
 // use standard TextFile + 64k buffer
 procedure BufferedTextFileWriter(const FileName: string);
 var
-  iteration, i: integer;
+  iteration, i: Integer;
   F: TextFile;
-  Buffer: array[word] of byte;
+  Buffer: array[Word] of Byte;
 begin
   AssignFile(F, FileName);
   ReWrite(F);
@@ -235,10 +243,10 @@ begin
 end;
 
 // simple way to write data with TCachedFileWriter
-// Write() method is like TStream.Write(Buffer, Size)
+// Write method is like TStream.Write(Buffer, Size)
 procedure SimpleCachedFileWriter(const FileName: string);
 var
-  iteration, i: integer;
+  iteration, i: Integer;
   Writer: TCachedFileWriter;
 begin
   Writer.Initialize(FileName);
@@ -247,20 +255,20 @@ begin
     for i := Low(STRINGS) to High(STRINGS) do
     with STRINGS[i] do
     begin
-      Writer.Write(pointer(S)^, Len);
-      Writer.Write(CRLF, sizeof(CRLF));
+      Writer.Write(Pointer(S)^, Len);
+      Writer.Write(CRLF, SizeOf(CRLF));
     end;
 
   finally
-    Writer.Finalize();
+    Writer.Finalize;
   end;
 end;
 
 // difficult but the fastest way to write data with TCachedFileWriter
-// you should use Current, Margin and Flush(). [optional additional memory]
+// you should use Current, Margin and Flush. [optional additional memory]
 procedure FastCachedFileWriter(const FileName: string);
 var
-  iteration, i: integer;
+  iteration, i: Integer;
   Writer: TCachedFileWriter;
 begin
   Writer.Initialize(FileName);
@@ -269,50 +277,48 @@ begin
     for i := Low(STRINGS) to High(STRINGS) do
     with STRINGS[i] do
     begin
-      if (Len <= sizeof(int64)) then
+      if (Len <= SizeOf(Int64)) then
       begin
-        // fast Move()
-        if (Len <= sizeof(integer)) then pinteger(Writer.Current)^ := pinteger(S)^
-        else pint64(Writer.Current)^ := pint64(S)^;
+        // fast Move
+        if (Len <= SizeOf(Integer)) then PInteger(Writer.Current)^ := PInteger(S)^
+        else PInt64(Writer.Current)^ := PInt64(S)^;
 
-        inc(NativeInt(Writer.Current), Len);
-        dec(Writer.Margin, Len);
+        Inc(NativeInt(Writer.Current), Len);
+        Dec(Writer.Margin, Len);
       end else
       begin
-        // smart Move()
+        // smart Move
         Writer.Write(pointer(S), Len);
       end;
 
       // CRLF
-      pword(Writer.Current)^ := CRLF_VALUE;
-      inc(NativeInt(Writer.Current), sizeof(word));
-      dec(Writer.Margin, sizeof(word));
+      PWord(Writer.Current)^ := CRLF_VALUE;
+      Inc(NativeInt(Writer.Current), SizeOf(Word));
+      Dec(Writer.Margin, SizeOf(Word));
 
       // Flush if needed
-      if (Writer.Margin <= 0) then Writer.Flush();
+      if (Writer.Margin <= 0) then Writer.Flush;
     end;
 
   finally
-    Writer.Finalize();
+    Writer.Finalize;
   end;
 end;
 
 
-
-
 // generate file name and note the time
-procedure RunBenchmarkProc(Description: string; Proc: TBenchmarkProc);
+procedure RunBenchmarkProc(const Description: string; const Proc: TBenchmarkProc);
 const
   FileName = 'out.txt';
 var
-  Time: dword;
+  Time: Cardinal;
 begin
-  inc(FileId);
-  ConsoleWrite('%d) "%s"... ', [FileId, Description], 0);
+  Inc(ProcNumber);
+  ConsoleWrite('%d) "%s"... ', [ProcNumber, Description], 0);
 
   Time := GetTickCount;
     Proc(FileName);
-  Time := GetTickCount-Time;
+  Time := GetTickCount - Time;
 
   ConsoleWrite('%dms', [Time]);
   CompareFiles(FileName, CORRECT_FILE_NAME);
@@ -320,31 +326,34 @@ end;
 
 
 begin
-try
-  ConsoleWrite('The benchmark shows how quickly you can write files', []);
-  ConsoleWrite('Destination file must be same as "correct_file.txt" (about 100Mb)', []);
-  ConsoleWrite('Test data generating... ', [], 0);
-  INITIALIZE_STRINGS();
-  if (not FileExists(CORRECT_FILE_NAME)) then BufferedTextFileWriter(CORRECT_FILE_NAME);
-  ConsoleWrite('done.', [], 3);
+  try
+    // Information and data generation
+    ConsoleWrite('The benchmark shows how quickly you can write files');
+    ConsoleWrite('Destination file must be same as "correct_file.txt" (about 100Mb)');
+    ConsoleWrite('Test data generating... ', 0);
+    INITIALIZE_STRINGS;
+    if (not FileExists(CORRECT_FILE_NAME)) then BufferedTextFileWriter(CORRECT_FILE_NAME);
+    ConsoleWrite('done.', 3);
 
-  // Run benchmark procs
-  ConsoleWrite('Let''s test methods (it may take an hour, but try to wait):', []);
-  RunBenchmarkProc('TextFile+Buffer', BufferedTextFileWriter);
-  RunBenchmarkProc('Simple CachedFileWriter', SimpleCachedFileWriter);
-  RunBenchmarkProc('Fast CachedFileWriter', FastCachedFileWriter);
-  RunBenchmarkProc('Standard TFileStream writer', FileStreamWriter);
-  RunBenchmarkProc('Standard TMemoryStream+SaveToFile()', MemoryStreamWriter);
+    // Run benchmark procs
+    ConsoleWrite('Let''s test write methods (it may take an hour, but try to wait):');
+    RunBenchmarkProc('TextFile+Buffer', BufferedTextFileWriter);
+    RunBenchmarkProc('Simple CachedFileWriter', SimpleCachedFileWriter);
+    RunBenchmarkProc('Fast CachedFileWriter', FastCachedFileWriter);
+    RunBenchmarkProc('Standard TFileStream writer', FileStreamWriter);
+    RunBenchmarkProc('Standard TMemoryStream+SaveToFile', MemoryStreamWriter);
 
-except
-  on E: Exception do
-  if (not (E is EStopBenchmark)) then
-  ConsoleWrite('%s: %s', [E.ClassName, E.Message]);
-end;
+  except
+    on EAbort do ;
+
+    on E: Exception do
+    ConsoleWrite('%s: %s', [E.ClassName, E.Message]);
+  end;
+
   if (ParamStr(1) <> '-nowait') then
   begin
     Writeln;
-    ConsoleWrite('Press Enter to quit', [], 0);
+    ConsoleWrite('Press Enter to quit', 0);
     Readln;
   end;
 end.
