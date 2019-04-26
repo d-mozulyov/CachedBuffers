06fca7a1dc90fe004eed0b20589e61a9d2b66961
d.mozulev <d.mozulev@osnovalab.ru>
2019-04-26 15:55:23 +0300

    License year fixed

--- sources/CachedStreams.pas
+++ sources/CachedStreams.pas
@@ -1,7 +1,7 @@
 unit CachedStreams;
 
 {******************************************************************************}
-{ Copyright (c) 2019 Dmitry Mozulyov                                           }
+{ Copyright (c) 2013-2019 Dmitry Mozulyov                                           }
 {                                                                              }
 { Permission is hereby granted, free of charge, to any person obtaining a copy }
 { of this software and associated documentation files (the "Software"), to deal}
